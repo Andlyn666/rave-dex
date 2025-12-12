11362822552f8cad640841396a8aa81aeb579dcf
Andlyn <andlyn666@126.com>
2025-12-12 19:19:38 +0800

    Update price_mgr.py

--- price_mgr.py
+++ price_mgr.py
@@ -46,8 +46,9 @@ def main():
         except Exception as e:
             logging.info("AerodromeV3Dex error: %s", e)
         try:
-            mark_price, index_price, funding_rate = get_latest_funding_rate('RAVEUSDT')
-            spot_price = get_latest_price_spot('RAVEUSDT')
+            mark_price, index_price, funding_rate = get_latest_funding_rate('RAVEUSD1')
+            spot_price = get_latest_price_spot('RAVEUSD1')
+            logging.info(f"Fetched funding rate: {funding_rate}, spot price: {spot_price}")
             upsert_penrose_cex_latest(
                 6, 'RAVE', spot_price, index_price, mark_price, funding_rate, now
             )
